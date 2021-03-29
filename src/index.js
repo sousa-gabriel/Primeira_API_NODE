fe3fa572dde0282285fc48f4f7158d0b34124b3f
Gabriel Rocha <sousa-gabriel@hotmail.com>
2021-03-29 20:52:23 -0300

    Validação de cpf duplicado no sistema

--- src/index.js
+++ src/index.js
@@ -7,12 +7,19 @@ const customers = []
 
 app.post('/account', (request, response)=>{
     const {cpf, name} = request.body;
-    const id = uuidv4();
+    const customersAlreadyExists = customers.some(
+        (customer) => customer.cpf ===cpf
+    );
+    if(customersAlreadyExists){
+        return response.status(400).json(
+            {error:"Customers already exists!"}
+        );
+    }
 
     customers.push({
         cpf,
         name,
-        id,
+        id:uuidv4(),
         statement:[]
     });
 
