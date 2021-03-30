c98937d6f8a76347db6d6f9b704a8d1801cd4ce6
Gabriel Rocha <sousa-gabriel@hotmail.com>
2021-03-29 21:46:19 -0300

    visualizar os dados do nosso estrato

--- src/index.js
+++ src/index.js
@@ -26,5 +26,15 @@ app.post('/account', (request, response)=>{
     return response.status(201).send();
 })
 
+app.get('/statement/:cpf', (request, response)=>{
+    const {cpf} = request.params;
+    
+    const customer = customers.find(customer => customer.cpf ===cpf);
+    if(!customer){
+        return response.json({error:"Customer not found"})
+    }
+    return response.json(customer.statement);
+})
+
 
 app.listen(3333)
